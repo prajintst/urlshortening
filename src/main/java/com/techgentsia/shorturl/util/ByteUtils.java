8aa6cfbee787fe90171269ede5c535a54e28bcc6
PRAJIN PRAKASH <prajin@techgentsia.com>
2021-10-13 11:16:06 +0530

    fix: optimize static reference

--- src/main/java/com/techgentsia/shorturl/util/ByteUtils.java
+++ src/main/java/com/techgentsia/shorturl/util/ByteUtils.java
@@ -3,15 +3,14 @@ package com.techgentsia.shorturl.util;
 import java.nio.ByteBuffer;
 
 public class ByteUtils {
-    private static ByteBuffer buffer = ByteBuffer.allocate(Integer.BYTES);
 
     public static byte[] intToBytes(int x) {
-        buffer.putInt(0, x);
-        return buffer.array();
+        return ByteBuffer.allocate(Integer.BYTES).putInt(0, x).array();
     }
 
     public static int bytesToInt(byte[] bytes) {
-        buffer.put(bytes, 0, bytes.length);
+        ByteBuffer buffer = ByteBuffer.allocate(Integer.BYTES)
+                .put(bytes, 0, bytes.length);
         buffer.flip();//need flip
         return buffer.getInt();
     }
